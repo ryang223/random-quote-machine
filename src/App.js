0ff7bb7d16b8438c7c4ba70162a569d4d0d8a410
Ryan <ryangoodWD@gmail.com>
2021-09-27 16:03:48 -0400

    functionality of random quote machine is completed

--- src/App.js
+++ src/App.js
@@ -2,40 +2,33 @@ import React, { useState } from 'react';
 import './App.scss';
 
 function App() {
-  const [quote, setQuote] = useState("Perceive that which cannot be seen with the eye.")
-  const [author, setAuthor] = useState("Miyamoto Musashi")
-
+  const [quote, setQuote] = useState("")
+  const [author, setAuthor] = useState("")
   const [randomNumber, setRandomNumber] = useState(0)
 
   const generateRandomNumber = () => {
-    let randomInteger = Math.floor(3 * Math.random())
+    let randomInteger = Math.floor(quotesArray.length * Math.random())
     setRandomNumber(randomInteger)
-    if (randomInteger === 0) {
-      setQuote(quotesArray[0])
-    }
-    if (randomInteger === 1) {
-      setQuote(quotesArray[1])
-      }
-    if (randomInteger === 2) {
-      setQuote(quotesArray[2])
-    }
+    setQuote(quotesArray[randomInteger].quote)
+    setAuthor(quotesArray[randomInteger].author)
+    
   }
 
   const quotesArray= [
 
-    "Do nothing which is of no use",
-
-    "Study strategy over the years and achieve the spirit of the warrior. Today is victory over yourself of yesterday; tomorrow is your victory over lesser men.",
-
-    "Perceive that which cannot be seen with the eye.",
-
-    "Perception is strong and sight weak. In strategy it is important to see distant things as if they were close and to take a distanced view of close things.",
-
-    "Engage in combat fully determined to die and you will be alive; wish to survive in the battle and you will surely meet death.",
-
-    "A samurai should always be prepared for death – whether his own or someone else’s.",
-
-    "Sword and mind must be united."
+    {quote:"Do nothing which is of no use",author:"Miyamoto Musashi"},
+    {
+           quote:"Study strategy over the years and achieve the spirit of the warrior. Today is victory over yourself of yesterday; tomorrow is your victory over lesser men.",author:"Miyamoto Musashi"},
+    {
+           quote:"Perceive that which cannot be seen with the eye.",author:"Miyamoto Musashi"},
+    {
+           quote:"Perception is strong and sight weak. In strategy it is important to see distant things as if they were close and to take a distanced view of close things.",author:"Miyamoto Musashi"},
+        {
+         quote: "Engage in combat fully determined to die and you will be alive; wish to survive in the battle and you will surely meet death.", author:"Uesugi Kenshin"},
+        {
+        quote:" A samurai should always be prepared for death – whether his own or someone else’s.",author:"Stan Sakai"},
+        {
+        quote:"Sword and mind must be united.",author:"Yamada Jirokichi"}
   ]
 
 
@@ -44,9 +37,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Random Number: {randomNumber}</h1>
-        <button onClick={() => generateRandomNumber()}> Generate A Random Number</button>
+        <button onClick={() => generateRandomNumber()}> Generate A Random Quote</button>
         <p>
-          "{quote}"
+          {quote}
         </p>
         <p> - {author}</p>
       </header>
